e9a14d6da838b9dced373765950200a081b65d79
TranDam23 <166279185+TranDam23@users.noreply.github.com>
2024-11-04 17:02:18 +0700

    update TraCuuKhachHang

--- Code/BanVeDTHP/src/gui/TraCuuKhachHang_GUI.java
+++ Code/BanVeDTHP/src/gui/TraCuuKhachHang_GUI.java
@@ -3,18 +3,30 @@ package gui;
 import java.awt.Color;
 import java.awt.SystemColor;
 
+import javax.swing.AbstractButton;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.RowFilter;
 import javax.swing.border.LineBorder;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.JScrollPane;
 import java.awt.Image;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 import components.ConTent_JPanel;
+import dao.KhachHang_DAO;
+import dao.NhanVien_DAO;
+import entity.KhachHang;
+import entity.NhanVien;
+import entity.TaiKhoan;
 
 import java.awt.Font;
 import java.awt.event.ActionListener;
@@ -22,43 +34,50 @@ import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
 import java.awt.event.ActionEvent;
 
-public class TraCuuKhachHang_GUI extends JPanel {
-	
+public class TraCuuKhachHang_GUI extends JPanel implements ActionListener {
+
 	private static final long serialVersionUID = 1L;
-	private JTextField textFieldCCCD;
-	private JTextField textField_MaKH;
-	private JTextField textField_TenKH;
-	private JTextField textField_Email;
-	private JTextField textField_SDT;
-	private JTable table_1;
-	/**
-	 * Create the panel.
-	 */
+	private JTextField txtCCCD;
+	private JTextField txtMaKH;
+	private JTextField txtTen;
+	private JTextField txtEmail;
+	private JTextField txtSDT;
+	private JTable table;
+	private DefaultTableModel model;
+	private JButton btn_Tim;
+	private KhachHang_DAO dskh = new KhachHang_DAO();
+	private TableRowSorter<TableModel> sorter;
+
 	public TraCuuKhachHang_GUI(TrangChu_GUI trangChu) {
-		
+
 		setBackground(SystemColor.text);
 		setForeground(new Color(255, 255, 255));
 		setBounds(0, 170, 1460, 570);
 		setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel.setBounds(10, 10, 1420, 560);
 		add(panel);
 		panel.setLayout(null);
-		
+
 		JPanel jp_QL = new JPanel();
 		jp_QL.setBounds(10, 10, 124, 28);
 		panel.add(jp_QL);
 		jp_QL.setLayout(null);
-		
+
 		JLabel lb_quaylai = new JLabel("Quay lại");
 		lb_quaylai.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lb_quaylai.setBounds(45, 0, 69, 27);
 		jp_QL.add(lb_quaylai);
-		
+
 		ImageIcon goBackIcon = new ImageIcon(getClass().getResource("/img/9054423_bx_arrow_back_icon.png"));
 		Image scaledGoBack = goBackIcon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
 		JLabel goBackIconLabel = new JLabel(new ImageIcon(scaledGoBack));
@@ -71,164 +90,208 @@ public class TraCuuKhachHang_GUI extends JPanel {
 			}
 		});
 		jp_QL.add(goBackIconLabel);
-		
+
 		JPanel panelThongTinKhachHang = new JPanel();
 		panelThongTinKhachHang.setBounds(10, 48, 337, 502);
 		panelThongTinKhachHang.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel.add(panelThongTinKhachHang);
 		panelThongTinKhachHang.setLayout(null);
-		
-		textField_MaKH = new JTextField();
+
+		txtMaKH = new JTextField();
 		String placeholder_maKH = "Theo mã khách hàng";
-        
-		textField_MaKH.setText(placeholder_maKH);
-		textField_MaKH.setForeground(Color.GRAY);
-		textField_MaKH.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		textField_MaKH.setBounds(35, 86, 274, 30);
-		panelThongTinKhachHang.add(textField_MaKH);
-		textField_MaKH.setColumns(10);
-		
-		textField_TenKH = new JTextField();
+
+		txtMaKH.setForeground(Color.GRAY);
+		txtMaKH.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtMaKH.setBounds(35, 86, 274, 30);
+		panelThongTinKhachHang.add(txtMaKH);
+		txtMaKH.setColumns(10);
+
+		txtTen = new JTextField();
 		String placeholder_TenKH = "Theo tên khách hàng";
-        
-		textField_TenKH.setText(placeholder_TenKH);
-		textField_TenKH.setForeground(Color.GRAY);
-		textField_TenKH.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		textField_TenKH.setColumns(10);
-		textField_TenKH.setBounds(35, 165, 274, 30);
-		panelThongTinKhachHang.add(textField_TenKH);
-		
-		textField_Email = new JTextField();
+
+		txtTen.setForeground(Color.GRAY);
+		txtTen.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtTen.setColumns(10);
+		txtTen.setBounds(35, 165, 274, 30);
+		panelThongTinKhachHang.add(txtTen);
+
+		txtEmail = new JTextField();
 		String placeholder_Email = "Theo email";
-        
-		textField_Email.setText(placeholder_Email);
-		textField_Email.setForeground(Color.GRAY);
-		textField_Email.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		textField_Email.setColumns(10);
-		textField_Email.setBounds(35, 234, 274, 30);
-		panelThongTinKhachHang.add(textField_Email);
-		
-		textField_SDT = new JTextField();
+
+		txtEmail.setForeground(Color.GRAY);
+		txtEmail.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtEmail.setColumns(10);
+		txtEmail.setBounds(35, 234, 274, 30);
+		panelThongTinKhachHang.add(txtEmail);
+
+		txtSDT = new JTextField();
 		String placeholder_SDT = "Theo số điện thoại";
-        
-		textField_SDT.setText(placeholder_SDT);
-		textField_SDT.setForeground(Color.GRAY);
-		textField_SDT.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		textField_SDT.setColumns(10);
-		textField_SDT.setBounds(35, 313, 274, 36);
-		panelThongTinKhachHang.add(textField_SDT);
-		
-		JButton btn_Tim = new JButton("Tìm");
-		btn_Tim.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btn_Tim.setBackground(Color.decode("#0091D4"));
-		btn_Tim.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		btn_Tim.setBounds(209, 449, 100, 30);
-		panelThongTinKhachHang.add(btn_Tim);
-		
+
+		txtSDT.setForeground(Color.GRAY);
+		txtSDT.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtSDT.setColumns(10);
+		txtSDT.setBounds(35, 313, 274, 36);
+		panelThongTinKhachHang.add(txtSDT);
+
 		JPanel panel_2 = new JPanel();
 		panel_2.setBounds(0, 0, 337, 36);
-		
+
 		panelThongTinKhachHang.add(panel_2);
 		panel_2.setBackground(new Color(51, 102, 153));
 		panel_2.setLayout(null);
-		
+
 		JLabel lb_Tim = new JLabel("Tìm kiếm");
 		lb_Tim.setForeground(new Color(255, 255, 255));
 		lb_Tim.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lb_Tim.setBounds(125, 0, 95, 36);
 		panel_2.add(lb_Tim);
-		
-		textFieldCCCD = new JTextField();
-        String placeholder = "Theo căn cước công dân";
-        
-        textFieldCCCD.setText(placeholder);
-        textFieldCCCD.setForeground(Color.GRAY);
-        textFieldCCCD.setToolTipText("");
-        textFieldCCCD.setFont(new Font("Tahoma", Font.PLAIN, 16));
-        textFieldCCCD.setColumns(10);
-        textFieldCCCD.setBounds(35, 392, 274, 36);
-		panelThongTinKhachHang.add(textFieldCCCD);
-		
+
+		txtCCCD = new JTextField();
+		String placeholder = "Theo căn cước công dân";
+
+		txtCCCD.setForeground(Color.GRAY);
+		txtCCCD.setToolTipText("");
+		txtCCCD.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtCCCD.setColumns(10);
+		txtCCCD.setBounds(35, 392, 274, 36);
+		panelThongTinKhachHang.add(txtCCCD);
+
 		JLabel lblCanCuuCongDan = new JLabel("Căn cước công dân:");
 		lblCanCuuCongDan.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblCanCuuCongDan.setBounds(35, 345, 150, 53);
 		panelThongTinKhachHang.add(lblCanCuuCongDan);
-		
+
 		JLabel lblMaKhachHang = new JLabel("Mã khách hàng:");
 		lblMaKhachHang.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblMaKhachHang.setBounds(35, 46, 150, 30);
 		panelThongTinKhachHang.add(lblMaKhachHang);
-		
+
 		JLabel lblTen = new JLabel("Tên khách hàng:");
 		lblTen.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblTen.setBounds(35, 126, 126, 29);
 		panelThongTinKhachHang.add(lblTen);
-		
+
 		JLabel lblEmail = new JLabel("Email:");
 		lblEmail.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblEmail.setBounds(35, 205, 100, 19);
 		panelThongTinKhachHang.add(lblEmail);
-		
+
 		JLabel lblSDT = new JLabel("Số điện thoại:");
 		lblSDT.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblSDT.setBounds(35, 284, 129, 19);
 		panelThongTinKhachHang.add(lblSDT);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
+
+		table = new JTable();
+		model = new DefaultTableModel(new Object[][] {}, new String[] { "STT", "Mã khách hàng", "Tên khách hàng",
+				"Email", "Số điện thoại", "Căn cước công dân" });
+		sorter = new TableRowSorter<>(model);
+
+		table.setRowSorter(sorter);
+		table.setModel(model);
+		scrollPane.setViewportView(table);
+		table.setRowHeight(25); // Set chiều cao hàng
+		table.setBounds(347, 10, 1053, 517);
 		scrollPane.setBounds(357, 10, 1053, 540);
 		panel.add(scrollPane);
-		
-		table_1 = new JTable();
-		table_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		table_1.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"STT", "Mã khách hàng", "Tên khách hàng", "Email", "Số điện thoại", "Căn cước công dân"
-			}
-		));
-		scrollPane.setViewportView(table_1);
-		table_1.setBounds(347, 10, 1053, 517);
-		
-//		 Thêm FocusListener
-//		textField_MaKH.addFocusListener(new FocusAdapter() {
-//            @Override
-//            public void focusGained(FocusEvent e) {
-//                if (textField_MaKH.getText().equals(placeholder_maKH)) {
-//                	textField_MaKH.setText("");
-//                	textField_MaKH.setForeground(Color.BLACK); // Đặt màu chữ khi nhập
-//                }
-//            }
-//
-//            @Override
-//            public void focusLost(FocusEvent e) {
-//                if (textField_MaKH.getText().isEmpty()) {
-//                	textField_MaKH.setForeground(Color.GRAY);
-//                	textField_MaKH.setText(placeholder_maKH); // Khôi phục lại placeholder
-//                }
-//            }
-//        });
-		
-        textFieldCCCD.addFocusListener(new FocusAdapter() {
-            @Override
-            public void focusGained(FocusEvent e) {
-                if (textFieldCCCD.getText().equals(placeholder)) {
-                    textFieldCCCD.setText("");
-                    textFieldCCCD.setForeground(Color.BLACK); // Đặt màu chữ khi nhập
-                }
-            }
 
-            @Override
-            public void focusLost(FocusEvent e) {
-                if (textFieldCCCD.getText().isEmpty()) {
-                    textFieldCCCD.setForeground(Color.GRAY);
-                    textFieldCCCD.setText(placeholder); // Khôi phục lại placeholder
-                }
-            }
-        });
-        
+		model.addTableModelListener(new TableModelListener() {
+			@Override
+			public void tableChanged(TableModelEvent e) {
+				int row = e.getFirstRow();
+				int column = e.getColumn();
+			}
+		});
+
+		btn_Tim = new JButton("Tìm");
+		btn_Tim.setBackground(Color.decode("#0091D4"));
+		btn_Tim.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btn_Tim.setBounds(209, 449, 100, 30);
+		panelThongTinKhachHang.add(btn_Tim);
+
+		btn_Tim.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		btn_Tim.addActionListener(this);
+		datatoTable();
 	}
+
+	public void datatoTable() {
+		dskh.reset();
+		ArrayList<KhachHang> list = dskh.docTuBang();
+		model = (DefaultTableModel) table.getModel();
+		model.setRowCount(0); // Xóa tất cả hàng trong bảng
+		int stt = 1; // Biến đếm bắt đầu từ 1 cho STT
+
+		for (KhachHang kh : list) {
+			model.addRow(new Object[] { stt++, kh.getMaKH(), kh.getTenKH(), kh.getCccd(), kh.getEmail(), kh.getSdt()
+
+			});
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object o = e.getSource();
+		dskh.reset();
+		if (o.equals(btn_Tim)) {
+			if (txtMaKH.getText() != null) {
+				filterRows();
+
+			}
+			if (txtTen.getText() != null) {
+				filterRows();
+
+			}
+
+			if (txtEmail.getText() != null) {
+				filterRows();
+
+			}
+			if (txtSDT.getText() != null) {
+				filterRows();
+
+			}
+			if (txtCCCD.getText() != null) {
+				filterRows();
+
+			}
+		}
+	}
+
+	private void filterRows() {
+		ArrayList<RowFilter<Object, Object>> filters = new ArrayList<>();
+		String maKH = txtMaKH.getText().trim();
+		String hoTen = txtTen.getText().trim();
+		String email = txtEmail.getText().trim();
+		String sdt = txtSDT.getText().trim();
+		String cccd = txtCCCD.getText().trim();
+		// Lọc theo các điều kiện
+		if (!maKH.isEmpty()) {
+			filters.add(RowFilter.regexFilter("(?i)" + maKH, 1));
+		}
+		if (!hoTen.isEmpty()) {
+			filters.add(RowFilter.regexFilter("(?i)" + hoTen, 2));
+		}
+		if (!email.isEmpty()) {
+			filters.add(RowFilter.regexFilter("(?i)" + email, 3));
+		}
+		if (!sdt.isEmpty()) {
+			filters.add(RowFilter.regexFilter("(?i)" + email, 4));
+		}
+		if (!cccd.isEmpty()) {
+			filters.add(RowFilter.regexFilter("(?i)" + email, 5));
+		}
+		// Cập nhật bộ lọc
+		if (filters.isEmpty()) {
+			sorter.setRowFilter(null); // Nếu không có bộ lọc nào, xóa bộ lọc
+		} else {
+			sorter.setRowFilter(RowFilter.andFilter(filters));
+		}
+	}
+	
+	
 }
