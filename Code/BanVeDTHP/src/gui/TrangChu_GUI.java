bd831fec6d61086c638cc9f097da2da7ae88425b
TranDam23 <166279185+TranDam23@users.noreply.github.com>
2024-12-03 06:23:04 +0700

    update dialog TaiKhoan

--- Code/BanVeDTHP/src/gui/TrangChu_GUI.java
+++ Code/BanVeDTHP/src/gui/TrangChu_GUI.java
@@ -9,6 +9,7 @@ import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 
 import components.ConTent_JPanel;
+import components.RoundedButton;
 import dao.Ca_DAO;
 import dao.NhanVien_DAO;
 import dao.TaiKhoan_DAO;
@@ -25,6 +26,7 @@ import java.awt.GraphicsEnvironment;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.RenderingHints;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
@@ -34,6 +36,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JMenu;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
 import java.awt.SystemColor;
@@ -44,11 +47,13 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JDialog;
 
 public class TrangChu_GUI extends JFrame implements ActionListener,MouseListener{
 
@@ -95,6 +100,9 @@ public class TrangChu_GUI extends JFrame implements ActionListener,MouseListener
 	public LocalDateTime vaoCa;
 	public LocalDateTime ketCa;
 	private int click = 0;
+	
+	private JPanel contentPane1;
+	private JLabel lb_TenNV,lb_MaNV,lb_NgaySinh,lb_CCCD,lb_Email,lb_SDT,lb_ChucVu,userIconLabel1;
 	
 	public TrangChu_GUI(DangNhap_GUI dangNhap) {
 		this.dangNhap = dangNhap;
@@ -287,10 +295,134 @@ public class TrangChu_GUI extends JFrame implements ActionListener,MouseListener
 	    userIconLabel.addMouseListener(new MouseAdapter() {
 	        @Override
 	        public void mouseClicked(MouseEvent e) {
-	            // Chuyển sang TaiKhoan_GUI
-	            TaiKhoan_GUI taiKhoanGUI = new TaiKhoan_GUI(dangNhap); // Khởi tạo cửa sổ mới
-	            taiKhoanGUI.setVisible(true); // Hiển thị cửa sổ mới
+	        	// Tạo một JDialog
+	            JDialog dialog = new JDialog((JFrame) SwingUtilities.getWindowAncestor(userIconLabel), "Thông tin tài khoản", true);
+	            dialog.setBounds(300, 300, 720, 388); 
 	            
+	            contentPane1 = new JPanel();
+	    		contentPane1.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+	    		setContentPane(contentPane);
+	    		contentPane1.setLayout(null);
+	    		
+	    		ImageIcon userIcon1 = new ImageIcon(getClass().getResource("/img/user.png"));
+	    	    Image scaledUser1 = userIcon1.getImage().getScaledInstance(73 ,56, Image.SCALE_SMOOTH); // Thay đổi kích thước logo
+	    	    userIconLabel1 = new JLabel(new ImageIcon(scaledUser1));
+	    	    userIconLabel1.setBounds(54, 84, 73, 56);
+	    		contentPane1.add(userIconLabel1);
+	    		
+	    		lb_TenNV = new JLabel("");
+	    		lb_TenNV.setText(nhanVien_DAO.getNhanVienTheoMaNV((taiKhoan_DAO.getTaiKhoanTheoMaTK(dangNhap.getTaiKhoanLogined().getMaTaiKhoan())).getNhanVien().getMaNV()).getTenNV());
+	    		lb_TenNV.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	    		lb_TenNV.setBounds(51, 172, 105, 25);
+	    		contentPane1.add(lb_TenNV);
+	    		
+	    		JLabel lblNewLabel_2 = new JLabel("Mã nhân viên:");
+	    		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	    		lblNewLabel_2.setBounds(227, 45, 105, 25);
+	    		contentPane1.add(lblNewLabel_2);
+	    		
+	    		JLabel lblNewLabel_2_1 = new JLabel("Ngày sinh:");
+	    		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	    		lblNewLabel_2_1.setBounds(227, 80, 105, 25);
+	    		contentPane1.add(lblNewLabel_2_1);
+	    		
+	    		JLabel lblNewLabel_2_1_1 = new JLabel("CCCD:");
+	    		lblNewLabel_2_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	    		lblNewLabel_2_1_1.setBounds(227, 115, 105, 25);
+	    		contentPane1.add(lblNewLabel_2_1_1);
+	    		
+	    		JLabel lblNewLabel_2_1_1_1 = new JLabel("Email:");
+	    		lblNewLabel_2_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	    		lblNewLabel_2_1_1_1.setBounds(227, 150, 105, 25);
+	    		contentPane1.add(lblNewLabel_2_1_1_1);
+	    		
+	    		JLabel lblNewLabel_2_1_1_1_1 = new JLabel("Số điện thoại:");
+	    		lblNewLabel_2_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	    		lblNewLabel_2_1_1_1_1.setBounds(227, 185, 105, 25);
+	    		contentPane1.add(lblNewLabel_2_1_1_1_1);
+	    		
+	    		JLabel lblNewLabel_2_1_1_1_1_1 = new JLabel("Chức vụ:");
+	    		lblNewLabel_2_1_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	    		lblNewLabel_2_1_1_1_1_1.setBounds(227, 220, 105, 25);
+	    		contentPane1.add(lblNewLabel_2_1_1_1_1_1);
+	    		
+	    		lb_MaNV = new JLabel();
+	    		lb_MaNV.setText(nhanVien_DAO.getNhanVienTheoMaNV((taiKhoan_DAO.getTaiKhoanTheoMaTK(dangNhap.getTaiKhoanLogined().getMaTaiKhoan()))
+	    				.getNhanVien().getMaNV()).getMaNV());
+	    		lb_MaNV.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	    		lb_MaNV.setBounds(362, 45, 200, 25);
+	    		contentPane1.add(lb_MaNV);
+	    		
+	    		lb_NgaySinh = new JLabel();
+	    		LocalDate ngaySinh = nhanVien_DAO.getNhanVienTheoMaNV(taiKhoan_DAO.getTaiKhoanTheoMaTK(dangNhap.getTaiKhoanLogined().getMaTaiKhoan())
+	    				.getNhanVien().getMaNV()).getNgaySinh();
+	    		// Định dạng ngày sinh
+	    		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	    		String ngaySinhFormatted = ngaySinh.format(formatter); // Chuyển LocalDate sang chuỗi theo định dạng
+	    		
+	    		lb_NgaySinh.setText(ngaySinhFormatted);
+	    		lb_NgaySinh.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	    		lb_NgaySinh.setBounds(362, 80, 200, 25);
+	    		contentPane1.add(lb_NgaySinh);
+	    		
+	    		lb_CCCD = new JLabel();
+	    		lb_CCCD.setText(nhanVien_DAO.getNhanVienTheoMaNV((taiKhoan_DAO.getTaiKhoanTheoMaTK(dangNhap.getTaiKhoanLogined().getMaTaiKhoan()))
+	    				.getNhanVien().getMaNV()).getCccd());
+	    		lb_CCCD.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	    		lb_CCCD.setBounds(362, 115, 200, 25);
+	    		contentPane1.add(lb_CCCD);
+	    		
+	    		lb_Email = new JLabel();
+	    		lb_Email.setText(nhanVien_DAO.getNhanVienTheoMaNV((taiKhoan_DAO.getTaiKhoanTheoMaTK(dangNhap.getTaiKhoanLogined().getMaTaiKhoan()))
+	    				.getNhanVien().getMaNV()).getEmail());
+	    		lb_Email.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	    		lb_Email.setBounds(362, 150, 200, 25);
+	    		contentPane1.add(lb_Email);
+	    		
+	    		lb_SDT = new JLabel();
+	    		lb_SDT.setText(nhanVien_DAO.getNhanVienTheoMaNV((taiKhoan_DAO.getTaiKhoanTheoMaTK(dangNhap.getTaiKhoanLogined().getMaTaiKhoan()))
+	    				.getNhanVien().getMaNV()).getSdt());
+	    		lb_SDT.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	    		lb_SDT.setBounds(362, 185, 200, 25);
+	    		contentPane1.add(lb_SDT);
+	    		
+	    		lb_ChucVu = new JLabel();
+	    		int chucVu = nhanVien_DAO.getNhanVienTheoMaNV(taiKhoan_DAO.getTaiKhoanTheoMaTK(dangNhap.getTaiKhoanLogined().getMaTaiKhoan())
+	    			        .getNhanVien().getMaNV()).getChucVu();
+	    		String tenChucVu;
+	    		switch (chucVu) {
+	    		    case 1:
+	    		        tenChucVu = "Quản lý";
+	    		        break;
+	    		    case 2:
+	    		        tenChucVu = "Nhân viên";
+	    		        break;
+	    		    default:
+	    		        tenChucVu = "Không xác định";
+	    		}
+	    		lb_ChucVu.setText(tenChucVu);
+	    		lb_ChucVu.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	    		lb_ChucVu.setBounds(362, 220, 200, 25);
+	    		contentPane1.add(lb_ChucVu);
+	    		
+	    		btn_VaoCa = new RoundedButton("Vào ca", 10);
+	    		btn_VaoCa.setForeground(new Color(255, 255, 255));
+	    		btn_VaoCa.setBackground(new Color(51, 102, 153));
+	    		btn_VaoCa.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	    		btn_VaoCa.setBounds(247, 271, 85, 21);
+	    		contentPane1.add(btn_VaoCa);
+	    		
+	    		btn_KetCa = new RoundedButton("Kết ca", 10);
+	    		btn_KetCa.setForeground(new Color(255, 255, 255));
+	    		btn_KetCa.setBackground(new Color(51, 102, 153));
+	    		btn_KetCa.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	    		btn_KetCa.setBounds(382, 271, 85, 21);
+	    		contentPane1.add(btn_KetCa);
+	    		
+	    		 dialog.setContentPane(contentPane1);
+	            // Hiển thị dialog
+	            dialog.setVisible(true);
 	        }
 	    });
 	    
